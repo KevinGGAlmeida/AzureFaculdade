cfcc8db1b5f72f29bb4f4d70b3abc1b647872301
kevin <gabriel_almeida50@yahoo.com.br>
2022-09-12 19:44:23 -0300

    Segunda Alteracao

--- init.py
+++ init.py
@@ -1,10 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return "Flask API get endpoint running"
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
